2c68de90751f76c4d5f7afa9be8b7d17c9bc538c
bruna <burna.caetano@acad.ufsm.br>
2023-09-27 10:07:10 -0300

    init project

---
+++ src/main/java/com/linguagensextensiveis/trabalhofinal/TrabalhoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.linguagensextensiveis.trabalhofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linguagensextensiveis/trabalhofinal/TrabalhoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linguagensextensiveis.trabalhofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
